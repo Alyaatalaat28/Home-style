6394c62f32d40363c9b702438e9d0ec5420149f1
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-10-27 10:15:02 +0200

    create user

--- lib/features/signUp/presentation/manager/cubit/sign_up_cubit.dart
+++ lib/features/signUp/presentation/manager/cubit/sign_up_cubit.dart
@@ -1,8 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home_style/features/signUp/data/user_model.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
   SignUpCubit() : super(SignUpInitial());
+
+ //create user
+  void craeteUser({
+    required String name,
+    required String userName, 
+    required String email,
+    required String uId}){
+      UserModel userModel=UserModel(
+        email:email ,
+        name:name  ,
+        userName:userName ,
+        uId:uId);
+
+        FirebaseFirestore.instance.collection('users').doc(uId).set(
+          userModel.toMap()).
+          then((value) {
+            emit(CreateUserSuccessState());
+          }).
+          catchError((error){
+            if (kDebugMode) {
+              print(error.toString());
+            }
+           emit(CreateUserErrorState());
+          });
+  }
+
+//signUp with email and password
+void signUPWithEmailAndPassword({
+    required String name,
+    required String userName, 
+    required String email,
+    required String password,
+}){
+  emit(SignUpLoadingState());
+  FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: email,
+    password: password).
+  then((value) {
+    craeteUser(
+      name:name , 
+      userName: userName,
+      email: email,
+      uId: value.user!.uid);
+  } ).
+  catchError((error){
+    if (kDebugMode) {
+      print(error.toString());
+    }
+    emit(SignUpErrorState());
+  });
+
+} 
+
+//SignUp with google
+
+
+
+
 }
--- lib/features/signUp/presentation/manager/cubit/sign_up_state.dart
+++ lib/features/signUp/presentation/manager/cubit/sign_up_state.dart
@@ -8,3 +8,15 @@ sealed class SignUpState extends Equatable {
 }
 
 final class SignUpInitial extends SignUpState {}
+
+class SignUpLoadingState extends SignUpState{}
+
+class SignUpSuccessState extends SignUpState{
+}
+class SignUpErrorState extends SignUpState{
+}
+
+class CreateUserSuccessState extends SignUpState{
+}
+class CreateUserErrorState extends SignUpState{
+}
